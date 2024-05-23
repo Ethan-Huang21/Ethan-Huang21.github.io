1690c48d5907fd29f8058deaed57e46fedd7a804
Ethan-Huang21 <ethan.huang@ucalgary.ca>
2024-05-23 02:02:18 -0600

    Sidebar Component

---
+++ js/script.js
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////
+// navlist toggles
+
+const navlist = document.querySelector(".main-nav-list");
+
+// navlist.addEventListener("mouseenter", function () {
+//   navlist.classList.toggle("active");
+// });
+
+// navlist.addEventListener('mouseleave', function() {
+//   navlist.classList.toggle("active");
+// })
+
+navlist.addEventListener("click", function () {
+  navlist.classList.toggle("active");
+});
+///////////////////////////////////////////////////////////
