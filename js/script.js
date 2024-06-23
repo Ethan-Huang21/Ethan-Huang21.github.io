21bc2359a2997b72e1c6cdc1579fdd8ecee22b09
Ethan-Huang21 <ethan.huang@ucalgary.ca>
2024-06-23 00:49:46 -0600

    Media Query Bugfix & Hover Sidebar Toggle

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@
 
 const navlist = document.querySelector(".main-nav-list");
 
-// navlist.addEventListener("mouseenter", function () {
-//   navlist.classList.toggle("active");
-// });
-
-// navlist.addEventListener('mouseleave', function() {
-//   navlist.classList.toggle("active");
-// })
-
-navlist.addEventListener("click", function () {
+navlist.addEventListener("mouseenter", function () {
   navlist.classList.toggle("active");
 });
+
+navlist.addEventListener('mouseleave', function() {
+  navlist.classList.toggle("active");
+})
+
+// navlist.addEventListener("click", function () {
+//   navlist.classList.toggle("active");
+// });
 ///////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////
